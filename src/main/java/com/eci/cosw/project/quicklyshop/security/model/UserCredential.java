d58ae4720a5fda296628313482f8ffb428cbb530
Fabian Ardila <fabianra97@gmail.com>
2018-10-28 02:33:22 -0500

    Configure UserCredential Repository and Refactoring UserCredential Class

--- src/main/java/com/eci/cosw/project/quicklyshop/security/model/UserCredential.java
+++ src/main/java/com/eci/cosw/project/quicklyshop/security/model/UserCredential.java
@@ -1,14 +1,40 @@
 package com.eci.cosw.project.quicklyshop.security.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class UserCredential {
+    
+    @Id
+    private String id;
+    
+    private String userName;
 
     private String hashedPassword;
 
     private String hashFunction;
 
-    public UserCredential(String hashedPassword, String hashFunction) {
+    public UserCredential(String userName, String hashedPassword, String hashFunction) {
+        this.userName = userName;
         this.hashedPassword = hashedPassword;
         this.hashFunction = hashFunction;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getHashedPassword() {
