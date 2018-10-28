2bccd9c6e22f2aa0f91361c94f7330ace8c5c5af
Fabian Ardila <fabianra97@gmail.com>
2018-10-28 00:35:51 -0500

    Configure user repository for user persistence

--- src/main/java/com/eci/cosw/project/quicklyshop/security/service/persistence/UserRepository.java
+++ src/main/java/com/eci/cosw/project/quicklyshop/security/service/persistence/UserRepository.java
@@ -3,6 +3,20 @@ package com.eci.cosw.project.quicklyshop.security.service.persistence;
 import com.eci.cosw.project.quicklyshop.security.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    User findUserById(long id);
+
+    User findUserByEmail(String email);
+
+    List<User> findUserByFirstName(String firstName);
+
+    List<User> findUserBySecondName(String secondName);
+
+    List<User> findUserByPhone(String phone);
+
+    List<User> findUserByAddress(String address);
 
 }
