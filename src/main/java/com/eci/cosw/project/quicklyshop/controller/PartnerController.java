126b5e671b9cb2135608cf7dfd3e76e43d504bc1
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-10-31 12:22:15 -0500

    base de rest de socios

---
+++ src/main/java/com/eci/cosw/project/quicklyshop/controller/PartnerController.java
@@ -0,0 +1,18 @@
+package com.eci.cosw.project.quicklyshop.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api")
+public class PartnerController {
+
+    private static final Logger logger = LogManager.getLogger(PartnerController.class);
+
+    // TODO: implementar
+
+}
